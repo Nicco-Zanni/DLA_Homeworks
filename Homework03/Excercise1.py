5ee7b73534b6f073657877b8458b7359b2a68d3c
Nicco-Zanni <niccozanni@gmail.com>
2026-05-01 18:59:49 +0200

    Implemented ex 1 and started ex 2

--- Homework03/Excercise1.py
+++ Homework03/Excercise1.py
@@ -4,11 +4,13 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import numpy as np
+import pygame
+import matplotlib.pyplot as plt
 from torch.distributions import Categorical
 
-env_render = gym.make('CartPole-v1', render_mode='human')
-env = gym.make('CartPole-v1')
-print(env.reset())
+#env_render = gym.make('CartPole-v1', render_mode='human')
+#env = gym.make('CartPole-v1')
+#print(env.reset())
 
 #togliere env tanto non viene utilizzato
 def select_action(obs, policy):
@@ -32,7 +34,7 @@ def compute_returns(rewards, gamma):
     return np.asarray(returns)
 
 # Given an environment and a policy, run it up to the maximum number of steps.
-def run_episode(env, policy, maxlen=500):
+def run_episode(env, policy, maxlen=500, device = "cpu"):
     # Collect just about everything.
     observations = []
     actions = []
@@ -43,8 +45,8 @@ def run_episode(env, policy, maxlen=500):
     (obs, info) = env.reset()
     for i in range(maxlen):
         # Get the current observation, run the policy and select an action.
-        obs = torch.tensor(obs)
-        (action, log_prob) = select_action(obs, policy)
+        obs_d = torch.as_tensor(obs, dtype=torch.float32).to(device)
+        (action, log_prob) = select_action(obs_d, policy)
         observations.append(obs)
         actions.append(action)
         log_probs.append(log_prob)
@@ -66,3 +68,130 @@ class PolicyNet(nn.Module):
         s = F.relu(self.fc1(s))
         s = F.softmax(self.fc2(s), dim=-1)
         return s
+    
+def evaluate_agent(env, policy, M, device):
+    total_rewards = 0.0
+    episodes_lenghts = 0.0
+    policy.eval()
+    for _ in range(M):
+        (observations, actions, log_probs, rewards) = run_episode(env, policy, device= device)
+        total_rewards += sum(rewards)
+        episodes_lenghts += len(rewards)
+    avg_rewards = total_rewards / M
+    avg_lenght = episodes_lenghts / M
+    return avg_rewards, avg_lenght
+    
+def reinforce(policy, env, N, M, env_render=None, gamma=0.99, num_episodes=10, baseline = False, device = "cpu"):
+    opt = torch.optim.Adam(policy.parameters(), lr=1e-2)
+
+    # Track episode rewards in a list.
+    running_rewards = [0.0]
+    avg_total_rewards = []
+    avg_episode_length = []
+    
+    # The main training loop.
+    policy.train()
+    for episode in range(num_episodes):
+        # Run an episode of the environment, collect everything needed for policy update.
+        (observations, actions, log_probs, rewards) = run_episode(env, policy, device =device)
+        
+        # Compute the discounted reward for every step of the episode. 
+        returns = torch.tensor(compute_returns(rewards, gamma), dtype=torch.float32).to(device)
+
+        if not episode % N:
+            avg_rewards, avg_lenght = evaluate_agent(env, policy, M, device)
+            avg_total_rewards.append(avg_rewards)
+            avg_episode_length.append(avg_lenght)
+            print(f'Average total reward: {avg_rewards }')
+            print(f'Average episode lenght: {avg_lenght}')
+            policy.train()
+            
+        
+        # Keep a running average of total discounted rewards for the whole episode.
+        running_rewards.append(0.05 * returns[0].item() + 0.95 * running_rewards[-1])
+        
+        # Standardize returns.
+        if baseline:
+            returns = (returns - returns.mean()) / returns.std()
+        
+        # Make an optimization step
+        opt.zero_grad()
+        loss = (-log_probs * returns).mean()
+        loss.backward()
+        opt.step()
+        
+        # Render an episode after every 100 policy updates.
+        if not episode % 100:
+            if env_render:
+                policy.eval()
+                run_episode(env_render, policy, device=device)
+                policy.train()
+            print(f'Running reward: {running_rewards[-1]}')
+    
+    # Return the running rewards.
+    policy.eval()
+    return running_rewards, avg_total_rewards, avg_episode_length
+
+def main():
+    # env_render = gym.make('CartPole-v1', render_mode='human')
+
+    # # Make a policy network and run a few episodes to see how well random initialization works.
+    # policy = PolicyNet(env_render)
+    # for _ in range(10):
+    #     run_episode(env_render, policy)
+    
+    # # If you don't close the environment, the PyGame window stays visible.
+    # env_render.close()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+    seed = 2112
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    env = gym.make('CartPole-v1')
+    env.reset(seed=seed)
+    env_render = None # gym.make('CartPole-v1', render_mode='human')
+    N = 100
+    M = 20
+    baseline = True
+
+    # Make a policy network.
+    policy = PolicyNet(env).to(device)
+
+    # Train the agent.
+    running_rewards, avg_rewards, avg_lengths = reinforce(policy, env, N, M, env_render, num_episodes=1000, baseline= baseline, device= device)
+    plt.figure()
+    plt.plot(running_rewards)
+    plt.title('Running Reward')
+    plt.xlabel('Episode')
+    plt.ylabel('Reward')
+    plt.savefig('running_reward_no_baseline_50.png')
+
+    plt.figure()
+    plt.plot(avg_rewards)
+    plt.title('Average Total Reward')
+    plt.xlabel('Evaluation (every N episodes)')
+    plt.ylabel('Avg Reward')
+    plt.savefig('avg_reward_no_baseline_50.png')
+
+    plt.figure()
+    plt.plot(avg_lengths)
+    plt.title('Average Episode Length')
+    plt.xlabel('Evaluation (every N episodes)')
+    plt.ylabel('Avg Length')
+    plt.savefig('avg_length_no_baseline_50.png') 
+    # Close up everything
+    #env_render.close()
+    env.close()
+
+    ren_seed = 1234
+    env_render = gym.make('CartPole-v1', render_mode='human')
+    env_render.reset(seed=seed)
+    avg_reward, avg_length = evaluate_agent(env_render, policy, 10, device)
+    env_render.close()
+    print(f'Average total reward: {avg_reward }')
+    print(f'Average episode lenght: {avg_length}')
+
+
+if __name__ == "__main__":
+    main()
